2583d542b7504f74bf4b1c711f073650bf0da9fa
Xu Wei <xu.wei.31415@gmail.com>
2020-04-07 09:38:50 -0700

    better draw
    draw mass by point size
    draw force by line

--- proj1/GridRenderer.cpp
+++ proj1/GridRenderer.cpp
@@ -23,22 +23,57 @@ layout (location = 3) in float mass;
 
 uniform mat4 projection;
 uniform mat4 view;
+uniform vec3 eye;
+
+out vec3 vPos;
+out vec3 vVec;
+out vec3 vColor;
 
 void main() {
     gl_Position = projection * view * vec4(pos, 1.0);
+//    gl_PointSize = mass * 100000 / length(eye - pos);
+
+    vPos = pos;
+    vVec = force / mass / 9.8 / 10;
+    vColor = vec3(1.0);
 }
 )");
 StaticStringSource fragment_src(R"(
 #version 450
 
+in vec3 vColor;
+
 out vec4 fragColor;
 
 void main() {
-    fragColor = vec4(1.0);
+    fragColor = vec4(vColor, 1.0);
 }
 )");
-std::unique_ptr<Shader> vertex_shader, fragment_shader;
-std::unique_ptr<Program> program;
+StaticStringSource geometry_src(R"(
+#version 450
+layout (points) in;
+layout (line_strip, max_vertices = 2) out;
+
+uniform mat4 projection;
+uniform mat4 view;
+
+in vec3 vPos[1];
+in vec3 vVec[1];
+
+out vec3 vColor;
+
+void main() {
+    gl_Position = projection * view * vec4(vPos[0], 1.0);
+    vColor = vec3(1.0);
+    EmitVertex();
+    gl_Position = projection * view * vec4(vPos[0] + vVec[0], 1.0);
+    vColor = vec3(0.0);
+    EmitVertex();
+    EndPrimitive();
+}
+)");
+std::unique_ptr<Shader> vertex_shader, fragment_shader, geometry_shader;
+std::unique_ptr<Program> program, vec_program;
 }  // namespace
 
 GridRenderer::GridRenderer(const std::vector<Particle>& particles)
@@ -67,7 +102,15 @@ GridRenderer::GridRenderer(const std::vector<Particle>& particles)
     program = Program::create();
     program->attach(vertex_shader.get(), fragment_shader.get());
     program->link();
+
+    geometry_shader = Shader::create(GL_GEOMETRY_SHADER, &geometry_src);
+    vec_program = Program::create();
+    vec_program->attach(vertex_shader.get(), fragment_shader.get(),
+                        geometry_shader.get());
+    vec_program->link();
   }
+
+  glEnable(GL_PROGRAM_POINT_SIZE);  // Point size indicates mass
 }
 
 void GridRenderer::Update(const std::vector<Particle>& particles) {
@@ -76,10 +119,17 @@ void GridRenderer::Update(const std::vector<Particle>& particles) {
 }
 
 void GridRenderer::Draw(const Camera& camera) {
+  vao->bind();
+
   program->use();
   program->setUniform("projection", camera.Projection());
   program->setUniform("view", camera.View());
+  program->setUniform("eye", camera.Eye());
+  vao->drawArrays(GL_POINTS, 0, size_);
 
-  vao->bind();
+  vec_program->use();
+  vec_program->setUniform("projection", camera.Projection());
+  vec_program->setUniform("view", camera.View());
+  vec_program->setUniform("eye", camera.Eye());
   vao->drawArrays(GL_POINTS, 0, size_);
 }
